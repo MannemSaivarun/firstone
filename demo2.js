c6190619e692f29bc81bbcab6502d23afffc2628
MannemSaivarun <mannemsaivarun992@gmail.com>
2023-07-02 12:39:02 +0530

    addsecondfile

--- demo2.js
+++
@@ -1,9 +0,0 @@
-document.title = "Sharpener";
-const minicontainer = document.getElementById("main");
-minicontainer.style.background ="#800000";
-
-const header = document.getElementById("header-title");
-header.style.borderBottom = "solid 5px 	#000000";
-
-document.getElementsByClassName("title")[0].style.fontWeight  = "bold";
-document.getElementsByClassName("title")[0].style.color = "green";
