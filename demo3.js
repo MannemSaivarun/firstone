77542a005441be2ce36f71c9cafe944a57ed0638
MannemSaivarun <mannemsaivarun992@gmail.com>
2023-07-03 09:34:04 +0530

    newChanges

--- demo3.js
+++ demo3.js
@@ -4,3 +4,9 @@ listItems[0].style.fontWeight = "bold";
 listItems[1].style.fontWeight = "bold";
 listItems[2].style.fontWeight = "bold";
 listItems[3].style.fontWeight = "bold";
+
+
+li = document.getElementsByTagName("li");
+for(i=0; i<li.length; i++){
+    li[i].style.backgroundColor = "#ccc";
+}
