aff7bd64a6e786850b4813e3761ea312a8a99b2d
MannemSaivarun <mannemsaivarun992@gmail.com>
2023-07-03 09:25:10 +0530

    changes

--- demo3.js
+++ demo3.js
@@ -1,3 +1,6 @@
 listItems = document.getElementsByClassName("list-group-item");
 listItems[2].style.backgroundColor = "green";
-listItems.style.fontWeight = "bold"
+listItems[0].style.fontWeight = "bold";
+listItems[1].style.fontWeight = "bold";
+listItems[2].style.fontWeight = "bold";
+listItems[3].style.fontWeight = "bold";
