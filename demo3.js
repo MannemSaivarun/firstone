5839c97d512a3b13399ff96aef653cb79ffad0d9
MannemSaivarun <mannemsaivarun992@gmail.com>
2023-07-04 12:48:19 +0530

    addednew

--- demo3.js
+++ demo3.js
@@ -11,16 +11,61 @@
 //     li[i].style.backgroundColor = "#ccc";
 // }
 //querySelector
-secondItem = document.querySelector(".list-group-item:nth-child(2)");
-secondItem.style.backgroundColor = "green";
+// secondItem = document.querySelector(".list-group-item:nth-child(2)");
+// secondItem.style.backgroundColor = "green";
 
-thirdItem = document.querySelector(".list-group-item:nth-child(3)");
-thirdItem.style.display = "none";
+// thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+// thirdItem.style.display = "none";
 
-//querySelectorALL
-liItem = document.querySelectorAll(".list-group-item");
-liItem[1].style.color = "green";
-li = document.querySelectorAll(".list-group-item:nth-child(odd)");
-for(i=0; i<li.length; i++){
-    li[i].style.backgroundColor = "green"
-}
+// //querySelectorALL
+// liItem = document.querySelectorAll(".list-group-item");
+// liItem[1].style.color = "green";
+// li = document.querySelectorAll(".list-group-item:nth-child(odd)");
+// for(i=0; i<li.length; i++){
+//     li[i].style.backgroundColor = "green"
+// }
+
+
+ItemList = document.querySelector("#items");
+//parentElement
+// console.log(ItemList.parentElement)
+// ItemList.parentElement.style.backgroundColor = "grey";
+
+//lastelementchild
+console.log(ItemList.lastElementChild);
+ItemList.lastElementChild.style.backgroundColor = "blue";
+
+//lastchild
+console.log(ItemList.lastChild);
+//ItemList.lastChild.style.backgroundColor = "blue";
+
+//firstelementchild
+main = document.querySelector("#main");
+console.log(main.firstElementChild)
+ItemList.firstElementChild.textContent = "hello"
+
+//firstChild
+console.log(ItemList.firstChild);
+
+//nextsibling
+console.log(ItemList.nextSibling);
+
+//nextelementsibling
+console.log(ItemList.nextElementSibling);
+//previoussibling
+console.log(ItemList.previousSibling);
+
+//previouselementsibling
+console.log(ItemList.previousElementSibling);
+ItemList.previousElementSibling.style.backgroundColor="brown";
+//createelement
+newId = document.createElement("id");
+console.log(newId);
+
+//setAttribute
+newId.setAttribute("name", "newid" );
+//createtesxtnode
+newIdTxt = document.createTextNode("hi this is new id");
+//appendchild
+newId.appendChild(newIdTxt);
+
