30ace9039d28749dda92dda4c2abe9e0fc3a260f
MannemSaivarun <mannemsaivarun992@gmail.com>
2023-07-05 10:38:42 +0530

    new file change

--- demo4.js
+++ demo4.js
@@ -1,5 +1,6 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 
 // Form submit event
@@ -8,6 +9,7 @@ form.addEventListener('submit', (e)=>{
   
     // Get input value
     var newItem = document.getElementById('item').value;
+    var newItem1 = document.getElementById('item1').value;
   
     // Create new li element
     var li = document.createElement('li');
@@ -15,6 +17,8 @@ form.addEventListener('submit', (e)=>{
     li.className = 'list-group-item';
     // Add text node with input value
     li.appendChild(document.createTextNode(newItem));
+
+    li.appendChild(document.createTextNode(newItem1));
   
     // Create del button element
     var deleteBtn = document.createElement('button');
@@ -37,18 +41,37 @@ form.addEventListener('submit', (e)=>{
     // Append li to list
     itemList.appendChild(li);
   });
+
+
 // Delete event
-itemList.addEventListener('click', removeItem);
-
-
-
-
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
+itemList.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+      }
     }
-  }
-}
+  });
+
+
+
+
+
+filter.addEventListener('keyup' ,(e) => {
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach((item) => {
+      var itemName = item.firstChild.textContent;
+      var itemName2 = item.childNodes[1].textContent;
+      
+      
+      if(itemName.toLowerCase().indexOf(text) != -1 || itemName2.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  } )
